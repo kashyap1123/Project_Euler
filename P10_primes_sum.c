08904fae4021c8f7561a9096f10781cd4afd4cb6
Manjunath Kashyap Jataprolu <kashyap1123@gmail.com>
2014-08-16 21:23:09 +0530

    Initial commit
    Initial commit

---
+++ P10_primes_sum.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<math.h>
+
+void main()
+{
+    long int iter, iter_2, running_sum, number_primes, N;
+    N   = pow(10, 6);
+    int arr[N];
+
+    // Fill up the array upfront
+    for( iter=0; iter<N; iter++ )
+    {
+        arr[iter]   = 1;
+    };
+    arr[0]  = 0;
+    arr[1]  = 0;
+
+    // Ignoring multiples of 2 and have running sum initially set to 2 (first prime!)
+    running_sum     = 2;
+    number_primes   = 1;
+
+    for( iter=2; iter<N; iter++ )
+    {
+        // This means we have a prime
+        if( arr[iter]!=0 )
+        {
+            running_sum     += ( 2 * iter ) - 1;
+            number_primes   += 1;
+            if( 2 * (2*iter - 1 ) > (2 * N - 1 ) )
+            {
+                for( iter_2=2; 2 * (2*iter_2 - 1 ) <= (2 * N - 1 ); iter_2++ )
+                {
+                    arr[iter_2] = 0;
+                    printf( "\niter is %d \t iter_2 is %d", iter, iter_2 );
+                };
+            };
+        };
+    };
+
+    // Print the result !
+    printf( "\n Sum of all the primes leq %d is %d. \n Number of primes is %d", N, running_sum, number_primes );
+}
